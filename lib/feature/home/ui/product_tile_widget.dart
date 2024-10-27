7309ce0c60e2b4d8bb8f71420376d3a2f89264bb
Dwaipayan Biswas <biswastatay73@gmail.com>
2024-10-27 06:05:38 +0530

    updated ui

--- lib/feature/home/ui/product_tile_widget.dart
+++ lib/feature/home/ui/product_tile_widget.dart
@@ -1,30 +1,86 @@
-
 import 'package:blocnew/feature/home/models/home_product_models.dart';
 import 'package:flutter/material.dart';
 
 class ProductTileWidget extends StatelessWidget {
   final ProductDataModel productDataModel;
-   ProductTileWidget({super.key, required this.productDataModel});
+
+  ProductTileWidget({super.key, required this.productDataModel});
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      margin: EdgeInsets.all(10),
-      padding: EdgeInsets.all(10),
+      margin: const EdgeInsets.all(10),
+      padding: const EdgeInsets.all(10),
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(10),
+        boxShadow: [
+          BoxShadow(
+            color: Colors.grey.withOpacity(0.2),
+            spreadRadius: 2,
+            blurRadius: 5,
+            offset: const Offset(0, 3),
+          ),
+        ],
+      ),
       child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           Container(
             height: 200,
-            width: double.maxFinite,
+            width: double.infinity,
             decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(10),
+              border: Border.all(color: Colors.grey[300]!),
               image: DecorationImage(
-                  image: NetworkImage(productDataModel.imageUrl)
-              )
+                fit: BoxFit.cover,
+                image: NetworkImage(productDataModel.imageUrl),
+              ),
             ),
-
           ),
-          Text(productDataModel.name),
-          Text(productDataModel.description),
+          const SizedBox(height: 15),
+          Text(
+            productDataModel.name,
+            style: const TextStyle(
+              fontSize: 18,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          const SizedBox(height: 8),
+          Text(
+            productDataModel.description,
+            style: TextStyle(color: Colors.grey[600]),
+          ),
+          const SizedBox(height: 10),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text(
+                "\$. ${productDataModel.price}",
+                style: const TextStyle(
+                  fontSize: 16,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.black,
+                ),
+              ),
+              Row(
+                children: [
+                  IconButton(
+                    onPressed: () {
+                      // Add Wishlist functionality here
+                    },
+                    icon: const Icon(Icons.favorite_border_outlined, color: Colors.black),
+                  ),
+                  IconButton(
+                    onPressed: () {
+                      // Add Cart functionality here
+                    },
+                    icon: const Icon(Icons.shopping_bag_outlined, color: Colors.black),
+                  ),
+                ],
+              ),
+            ],
+          ),
         ],
       ),
     );
